b14e3154e131dd724835b93ad5182a32d4b21bf3
Casahel <68067947+Casahel@users.noreply.github.com>
2023-02-25 01:25:19 +0100

    25.02 - App dir now works, useContext for theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
-import { Brand } from "@/components/shared/Brand/Brand";
 import "../styles/globals.css";
 
 export default function Page() {
-    return (
-        <>
-            <Brand />
-        </>
-    );
+    return <h1>Home</h1>;
 }
--- components/shared/NavElement/NavElement.tsx
+++ components/shared/NavElement/NavElement.tsx
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./NavElement.module.css";
+import themes from "@/styles/theme.module.css";
 import { cormorant } from "@/lib/fonts";
 import { NavElementProps } from "@/types";
+import { ThemeContext } from "@/app/ThemeContex";
 
 export const NavElement = (props: NavElementProps) => {
     const { imgSrc, label, destination } = props;
-
     const element = imgSrc ? (
         <Link
             className={styles.wrap}
             href={destination ? destination : ""}
             passHref>
             <Image
-                className={styles.image}
+                className={`${styles.image}`}
                 src={imgSrc}
                 width={38}
                 height={48}
