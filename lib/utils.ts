ca73abaffc64f56907cb1c3fce53c7897f200ce6
Casahel <68067947+Casahel@users.noreply.github.com>
2023-04-28 20:14:29 +0200

    28/04 - Safari scroll debug

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,7 +7,7 @@ export const scrollToTop = () => {
     //the below is a workaround for a known iOS Safari scrolling bug.
     //See: https://github.com/vercel/next.js/issues/37141
     setTimeout(() => {
-        window.scrollTo(0, 0); // scroll to top again after 25ms!
+        window.scroll({ top: -1, left: 0, behavior: "smooth" }); // scroll to top again after 25ms!
     }, 25);
     // This should be any number larger than 16.8
 };
